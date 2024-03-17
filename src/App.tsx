e6a11d975afc072806382df5e28b312493f3b289
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-03-17 12:35:23 +0100

    set up weahter api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weatherData, setWeatherData] = useState({});
+
+  const APY_KEY = process.env.VITE_API_KEY
+
+  useEffect(() => {
+    if(APY_KEY !== undefined) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${APY_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => setWeatherData(data))
+        .catch((error) => console.error("Error fetching data: ", error));
+    }
+    
+  }, [APY_KEY]);
+
+  console.log(weatherData)
 
   return (
     <>
-      <div className='bg-blue-dark'>
+      <div className="bg-blue-dark">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,20 +32,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
